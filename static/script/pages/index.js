7fbd73f752b9702a2cebc78a59e6e2a7b0c5f15e
yanhua1117 <yh_rw828@163.com>
2017-10-18 23:31:49 +0800

    11:31

--- static/script/pages/index.js
+++ static/script/pages/index.js
@@ -10,7 +10,25 @@ $.get('ajax/index',function(d){
 			female:d.items[3].data.data,
 			male:d.items[4].data.data,
 			free:d.items[5].data.data,
-			topic:d.items[6].data.data
+			topic:d.items[6].data.data,
+			duration:0,
+			position:0,
+			header_position:0,
+			header_duration:0,
+			tab_1_class:'Swipe-tab_on',
+			tab_2_class:''
+		},
+		methods:{
+			tabSwitch:function(pos){
+				this.duration = 0.5;
+				this.header_duration = 0.5;
+				if( pos == 0 ){
+					this.position = 0;
+					this.header_position = 0;
+					this.tab_1_class = "Swipe-tab_on";
+					this.tab_2_class = "";
+				}
+			}
 		}
 	})
 },'json');
